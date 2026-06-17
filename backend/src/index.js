5353d4134964daffb080cf1aa90c4c0b65da15bd
Carlos Salazar <carlosjsoft@gmail.com>
2026-06-17 10:11:46 -0500

    ajuste para validar

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
+const { app } = require('@azure/functions');
+
 console.log('INDEX LOADED');
-require('./functions/auth');
-require('./functions/catalog');
-require('./functions/locations');
-require('./functions/orders');
-require('./functions/lists');
-require('./functions/admin');
-require('./functions/adminBulk');
-require('./functions/adminImages');
-require('./functions/adminOrders');
-console.log('ORDERS LOADED');
+
+app.http('health', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    route: 'health',
+    handler: async () => {
+        return {
+            status: 200,
+            body: 'OK'
+        };
+    }
+});
