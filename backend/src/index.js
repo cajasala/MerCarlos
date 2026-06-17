5d4dcaf61b8a2e3997933ff7ad6b7cbe9b0e4597
Carlos Salazar <carlosjsoft@gmail.com>
2026-06-17 16:05:26 -0500

    Ajustes 14

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,11 +27,9 @@ require('./functions/lists');
 console.log('666666666');
 require('./functions/admin');
 console.log('777777777');
-/*
 require('./functions/adminBulk');
 console.log('8888888888');
 require('./functions/adminImages');
 console.log('9999999999');
 require('./functions/adminOrders');
 console.log('101010101010');
-*/
