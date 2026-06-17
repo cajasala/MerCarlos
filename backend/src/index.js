705eeb0f2055be0ad24cd1d8d75353ef5f75a788
Carlos Salazar <carlosjsoft@gmail.com>
2026-06-17 10:49:34 -0500

    Ajuste seguimiento 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,4 +14,22 @@ app.http('health', {
     }
 });
 
+console.log('1111111');
 require('./functions/auth');
+console.log('2222222');
+require('./functions/catalog');
+console.log('3333333');
+require('./functions/locations');
+console.log('44444444');
+require('./functions/orders');
+console.log('555555555');
+require('./functions/lists');
+console.log('666666666');
+require('./functions/admin');
+console.log('777777777');
+require('./functions/adminBulk');
+console.log('8888888888');
+require('./functions/adminImages');
+console.log('9999999999');
+require('./functions/adminOrders');
+console.log('101010101010');
